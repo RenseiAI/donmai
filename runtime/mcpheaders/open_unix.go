b1c7fd8d6193e2747a756b8148ca242205f3397d
Mark Kropf <markkropf@gmail.com>
2026-09-17 01:12:27 -0400

    mcp: open bearer files through Unix root

--- runtime/mcpheaders/open_unix.go
+++ runtime/mcpheaders/open_unix.go
@@ -3,7 +3,9 @@
 package mcpheaders
 
 import (
+	"errors"
 	"os"
+	"path/filepath"
 
 	"golang.org/x/sys/unix"
 )
@@ -11,11 +13,22 @@ import (
 var openBearerFile = openBearerFileNoFollow
 
 func openBearerFileNoFollow(path string) (*os.File, os.FileInfo, error) {
-	fd, err := unix.Open(path, unix.O_RDONLY|unix.O_CLOEXEC|unix.O_NOFOLLOW|unix.O_NONBLOCK, 0)
+	// os.Root.OpenFile routes these native flags through the standard-library
+	// Unix opener, which adds close-on-exec while returning the owned *os.File.
+	// O_NOFOLLOW authenticates the opened pathname object and
+	// O_NONBLOCK prevents a substituted FIFO/device from hanging before fstat.
+	root, err := os.OpenRoot(filepath.Dir(path))
 	if err != nil {
 		return nil, nil, err
 	}
-	file := os.NewFile(uintptr(fd), path) //nolint:gosec // G115: successful unix.Open returns a nonnegative OS descriptor representable as uintptr.
+	file, err := root.OpenFile(filepath.Base(path), os.O_RDONLY|unix.O_NOFOLLOW|unix.O_NONBLOCK, 0)
+	closeRootErr := root.Close()
+	if err != nil || closeRootErr != nil {
+		if file != nil {
+			_ = file.Close()
+		}
+		return nil, nil, errors.Join(err, closeRootErr)
+	}
 	info, err := file.Stat()
 	if err != nil {
 		_ = file.Close()
